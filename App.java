107558f2388692de5d6baf9d71a76cf08e592e43
Wadee-Alhendi <112881741+Wadee-Alhendi@users.noreply.github.com>
2022-09-28 11:10:44 +0300

    Add files via upload

---
+++ App.java
@@ -0,0 +1,20 @@
+package edu.uqu.cs;
+
+/**
+ * This a Maven based Java project.
+ * Fix this by:
+ * 1. Rename the class name from FirstExercise to App
+ * 2. Add the public keyword before the class keyword
+ * 3. Fix the main mehtod declaration
+ * 4. Before submiting your code, modify the text “Hello World” to “Hello: <Name>, <Student ID>” in App.java
+ *      a.	Change <Name> to your name
+ *      b.	Change <Student ID> to your student ID
+ */
+ 
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello: Wadee Waleed Alhendi, s442010958" );
+    }
+}
